030ed379b53686e8dfc1e6ee8df9822a1223bf7e
Aadarsh032 <kumarn206@gmail.com>
2024-07-22 20:57:50 +0530

    installed tailwind

--- Sarswati Construction/src/App.jsx
+++ Sarswati Construction/src/App.jsx
@@ -1,10 +1,12 @@
-import './App.css'
+import React from 'react'
+import Home from "./Components/Home/Home"
+import AboutUs from "./Components/AboutUs/AboutUs"
 
 function App() {
 
   return (
     <>
-     
+       <Home/>
     </>
   )
 }
---
+++ Sarswati Construction/src/Components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className='homepage'>
+         <div className="bg-blue-500 h-screen flex items-center justify-center">
+      <h1 className="text-white text-3xl">Hello, Tailwind!</h1>
+      <p className=' bg-gray-500 '>sdfgsf</p>
+    </div>
+    </div>
+  )
+}
+
+export default Home
