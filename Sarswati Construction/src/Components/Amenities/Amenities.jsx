d4fda90dab76d044502e45dc8bcab552bc2d527d
Aadarsh032 <kumarn206@gmail.com>
2024-07-24 13:16:16 +0530

    Contact  Section

--- Sarswati Construction/src/Components/Amenities/Amenities.jsx
+++ Sarswati Construction/src/Components/Amenities/Amenities.jsx
@@ -8,7 +8,7 @@ const Amenities = () => {
   return (
     <div className='amenities bg-[#F5F5F5]  pb-28 pt-7 flex flex-col gap-24' >
 
-      <div className='amenities-top flex flex-row w-[95%] m-auto gap-8 text-start ml-24 pt-24 '>
+      <div className='amenities-top flex flex-row w-[95%] m-auto gap-8 text-start ml-[92px] pt-24 '>
         <div className='w-1/4 text-5xl font-semibold  flex items-center'><p className=''>Our best Amenities</p> </div>
         <p className='w-2/4 text-[22px] font-semibold '>Nestled in the serene outskirts of Ranchi, Rose Avenue is our testament to luxury and design. This prestigious project offers a blend of modern amenities and natural beauty, providing an unparalleled living experience.</p>
         <div className='w-[20%] flex items-center justify-center'> <button className='text-xl font-bold bg-[#DF7F8E] flex flex-row items-center max-w-max gap-4 p-2 px-8 rounded-3xl justify-end' style={{ color: '#FFFFFF' }} >MORE <img src={whitearrow} alt="" /></button></div>
