eb16bffe2c2bbace025e866787257a36be0e05a2
Aadarsh032 <kumarn206@gmail.com>
2024-07-25 20:00:37 +0530

    Animation Using Framer Done

--- Sarswati Construction/src/Components/Projects/Projects.jsx
+++ Sarswati Construction/src/Components/Projects/Projects.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import projects from '../../assets/projects'
 import IndividualProject from './IndividualProject/IndividualProject'
-
 import AnimateFadeIn from '../Animate/AnimateFade/AnimateFadeIn';
 
 
@@ -10,7 +9,7 @@ const Projects = () => {
 
   return (
     <div className='projects-mainbox bg-[#F5F5F5]' id='Projects'>
-      <div className='project-insidebox flex w-11/12 justify-between justify-around m-auto pb-20 pt-8'>
+      <div className='project-insidebox flex w-11/12 justify-evenly m-auto pb-20 pt-8'>
 
         {
           projects.map((element, index) => {
