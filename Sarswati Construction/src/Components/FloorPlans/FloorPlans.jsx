7f26cb79de34399fd3b1439328b8fad20068739a
Aadarsh032 <kumarn206@gmail.com>
2024-08-05 11:21:21 +0530

    FloorMaps Section Updated

--- Sarswati Construction/src/Components/FloorPlans/FloorPlans.jsx
+++ Sarswati Construction/src/Components/FloorPlans/FloorPlans.jsx
@@ -3,6 +3,8 @@ import left_building_image from '../../assets/Left-building-image.png'
 import floormapground from '../../assets/Ground Floor Plan-.jpg'
 import floormap1 from '../../assets/1st floor plan-.jpg'
 import floormap2 from '../../assets/2nd floor plan-.jpg'
+import floormapeven from '../../assets/4,6,8,10,12,14 floor plan-.jpg'
+import floormapodd from '../../assets/3,5,7,9,11,13 floor plan-.jpg'
 import FloorLayout from './FloorLayout/FloorLayout';
 import { useState } from 'react';
 import AnimateUpDown from '../Animate/AnimateUpDown/AnimateUpDown'
@@ -27,6 +29,14 @@ const FloorPlans = () => {
     setFloor("SECOND FLOOR");
     setimage(floormap2);
   }
+  function oddfloor() {
+    setFloor("ODD FLOOR");
+    setimage(floormapodd);
+  }
+  function evenfloor() {
+    setFloor("EVEN FLOOR");
+    setimage(floormapeven);
+  }
 
 
   return (
@@ -43,11 +53,13 @@ const FloorPlans = () => {
           <p className='text-[17px] font-thin max-xl:text-[15px]  max-sm:text-xl'>Discover the thoughtfully designed floor plans of Rose Avenue, crafted to maximize space, comfort, and convenience. Each layout is tailored to meet the diverse needs of our residents, ensuring an ideal living experience for every family.</p>
         </div>
         </AnimateUpDown>
-        <div className='floorplans-right-bottom flex flex-row gap-4 max-sm:flex-col-reverse max-sm:text-center '>
-          <div className='floorplans-right-bottom-left list-none text-xl flex flex-col gap-4 font-semibold  max-xl:text-xs max-lg:text-[10px] max-sm:text-xl ' >
+        <div className='floorplans-right-bottom flex flex-row gap-4 max-lg:flex-col  max-md:flex-col-reverse  max-sm:text-center '>
+          <div className='floorplans-right-bottom-left list-none text-xl flex flex-col gap-4 font-semibold  max-xl:text-xs max-lg:flex-row max-md:flex-col max-lg:text-[10px] max-sm:text-xl ' >
             <li className={`p-2 cursor-pointer ${floor === "GROUND FLOOR" ? 'bg-[#D9D9D9]  animate-fadeIn ' : 'bg-white'} `} onClick={groundfloor} >GROUND FLOOR</li>
             <li className={`p-2 cursor-pointer ${floor === "FIRST FLOOR" ? ' bg-[#D9D9D9]  animate-fadeIn ' : 'bg-white'} `} onClick={firstfloor} >FIRST FLOOR</li>
             <li className={`p-2 cursor-pointer ${floor === "SECOND FLOOR" ? ' bg-[#D9D9D9]  animate-fadeIn ' : 'bg-white'} `} onClick={secondfloor} >SECOND FLOOR</li>
+            <li className={`p-2 cursor-pointer ${floor === "ODD FLOOR" ? ' bg-[#D9D9D9]  animate-fadeIn ' : 'bg-white'} `} onClick={oddfloor} >3,5,7,9,11,13 FLOORS</li>
+            <li className={`p-2 cursor-pointer ${floor === "EVEN FLOOR" ? ' bg-[#D9D9D9]  animate-fadeIn ' : 'bg-white'} `} onClick={evenfloor} >4,6,8,10,12,14 FLOORS</li>
           </div>
           <div className='floorplans-right-bottom-left border-[1px] cursor-move '>
             <FloorLayout image={image} />
